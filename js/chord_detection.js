2d0f8525f5d883562fe2931a018302870dc4a605
John Pucciarelli <john.pucciarelli@gmail.com>
2013-05-18 13:37:41 -0400

    added switch for animation modes

--- js/chord_detection.js
+++ js/chord_detection.js
@@ -2,15 +2,19 @@ var ChordDetect = function(options) {
 
 	var _cd = {};
 
-	var _context;
+	var _audio;
 	var _analyser;
 	var _microphone;
 	var _volume;
+	var _canvas;
+	var _canvasContext;
 
 	var _defaults = {
 		volume: 0.0,
 		fftSize: 1024,
-		smoothing: 0
+		smoothing: 0,
+		mode: 'spectrum',
+		canvasId: 'canvas'
 	};
 
 	var _attrs = _.defaults(options, _defaults);
@@ -22,32 +26,32 @@ var ChordDetect = function(options) {
 	_cd.listen = function() {
 
 		if (typeof AudioContext !== 'undefined') {
-			_context = new AudioContext();
+			_audio = new AudioContext();
 		} else if (typeof webkitAudioContext !== 'undefined') {
-			_context = new webkitAudioContext();
+			_audio = new webkitAudioContext();
 		} else {
 			_error('Audio context not supported!');	
 		}
 	
-		if (_context) {
+		if (_audio) {
 
-			_analyser = _context.createAnalyser();
+			_analyser = _audio.createAnalyser();
 			_analyser.smoothingTimeConstant = _attrs.smoothing;
 			_analyser.fftSize = _attrs.fftSize;
 			
-			var gainNode = _context.createGainNode();
+			var gainNode = _audio.createGainNode();
 			_volume = gainNode.gain;
 			_cd.setVolume(_attrs.volume);
 
 			navigator.webkitGetUserMedia( { audio: true }, function(stream) {
 
-				_microphone = _context.createMediaStreamSource(stream);
+				_microphone = _audio.createMediaStreamSource(stream);
 				_microphone.connect(_analyser);
 
 				_analyser.connect(gainNode);
-				gainNode.connect(_context.destination);
+				gainNode.connect(_audio.destination);
 
-				_cd.animate();
+				_cd.startAnimation(_attrs.mode);
 
 			 },  _error('failed to get user media.') );
 			
@@ -55,31 +59,48 @@ var ChordDetect = function(options) {
 
 	};
 
-	_cd.animate = function() {
+	_cd.startAnimation = function(mode) {
 
-		var canvas = document.getElementById('canvas');
+		_canvas = document.getElementById(_attrs.canvasId);
 
-		if (canvas.getContext) {
+		if (_canvas.getContext) { 
 
-			var context = canvas.getContext('2d');
-						
-			var requestAnimationFrame = window.requestAnimationFrame ||
+			_canvasContext = _canvas.getContext('2d');
+			_requestAnimationFrame = window.requestAnimationFrame ||
 				window.webkitRequestAnimationFrame ||
 				window.mozRequestAnimationFrame ||
 				window.msRequestAnimationFrame ||
 				window.oRequestAnimationFrame ||
 				function(callback) {
 					return setTimeout(callback, 1);
-			};
+				};
+
+
+			switch (mode) {
+
+				case 'spectrum':
+					_cd.animateSpectrum();
+				break;
+
+				case 'spectrogram':
+				default:
+					_cd.animateSpectrogram();
+				break;
+			}
+
+		}
+
+	};
+
+	_cd.animateSpectrum = function() {
 
 			var draw = function() {
 
-				context.fillStyle = 'rgba(255, 255, 255, 0.01)';
-				context.beginPath();
-				context.rect(0, 0, canvas.width, canvas.height);
-				context.fill();
-				//context.clearRect(0, 0, canvas.width, canvas.height);
-				context.fillStyle = 'rgb(' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ')';
+				_canvasContext.fillStyle = 'rgba(255, 255, 255, .01)';
+				_canvasContext.beginPath();
+				_canvasContext.rect(0, 0, canvas.width, canvas.height);
+				_canvasContext.fill();
+				_canvasContext.fillStyle = 'rgb(' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ')';
 
 				var blockWidth = canvas.width / (_attrs.fftSize / 2);
 				var blockHeight = canvas.height / 256;
@@ -88,9 +109,9 @@ var ChordDetect = function(options) {
 				_analyser.getByteFrequencyData(frequencyArray);
 
 				for (var i = 0; i < frequencyArray.length; i++) {
-					context.beginPath();
-					context.rect(i * blockWidth, canvas.height, blockWidth, -1 * frequencyArray[i] * blockHeight); 
-					context.fill();
+					_canvasContext.beginPath();
+					_canvasContext.rect(i * blockWidth, canvas.height, blockWidth, -1 * frequencyArray[i] * blockHeight); 
+					_canvasContext.fill();
 				}
 
 			requestAnimationFrame(draw);
@@ -98,8 +119,6 @@ var ChordDetect = function(options) {
 			};
 
 			draw();
-
-		}
 
 	};
 
